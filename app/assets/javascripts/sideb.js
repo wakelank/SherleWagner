c24fb593738bb1bd0abd577acaec1536b8395a2f
paulh1am <pshiam@gmail.com>
2015-10-23 16:56:55 -0400

    sidebar prod types and classes work

---
+++ app/assets/javascripts/sideb.js
@@ -0,0 +1,55 @@
+
+
+
+
+$(document).ready(function(){
+
+
+  var checkboxer = function(clickedBox){
+//do check box array stuff here
+//then call the dom show/hide function here
+
+};
+
+// $( "#Fittings" ).click(function(){
+//   console.log("clicked Fittings");
+//   $(document.getElementsByClassName("prod_tile")).each(function(i){this.style.display='none';
+
+
+//     $(jsonproducts.product_groups).each(function(index){
+//       if (this.product_type.name == "FITTINGS"){
+//         var domPro = document.getElementById(this.id);
+//         console.log(domPro);
+//         domPro.style.display = 'block';
+//       }
+
+//     });
+
+//   });
+// });
+
+//   $( "#Fixtures" ).click(function(){
+//     console.log("clicked Fixtures");
+//     $(document.getElementsByClassName("prod_tile")).each(function(i){this.style.display='none';
+
+//       $(jsonproducts.product_groups).each(function(index){
+//         if (this.product_type.name == "FIXTURES"){
+//           var domPro = document.getElementById(this.id);
+//           console.log(domPro);
+//           domPro.style.display = 'block';
+//         }
+
+//       });
+
+//   });
+// });
+
+
+
+
+});
+
+
+
+
+
