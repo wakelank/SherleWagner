a6213082f5ead2269244a50395534912dd8109b5
wakelank <wakelank@gmail.com>
2015-12-04 16:23:29 -0500

    weird pull

---
+++ app/sidebScript.rb
@@ -0,0 +1,23 @@
+Thermostatic Shower Trims 
+ Concentric Thermostatic Trims 
+ Volume on/off & Diverter Control Trims 
+ Shower Heads 
+ Rain Domes 
+ Body Sprays 
+ Rain Bars 
+ Hand Showers 
+ Hand Shower Accessories 
+ Deck Mount Tub Spouts 
+ Wall Mount Tub Spouts 
+ Waste Assembly 
+ Shower Arm Diverters 
+
+
+ def format(arr)
+  arr.each do |e|
+    
+
+  end
+
+
+ end
