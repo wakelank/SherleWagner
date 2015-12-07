0d11605ef68191cdb8a98ee1b7c3b76759a8b6e0
paulh1am <pshiam@gmail.com>
2015-12-07 11:44:06 -0500

    pre monday pull

--- app/sidebScript.rb
+++ app/sidebScript.rb
@@ -13,11 +13,18 @@ Thermostatic Shower Trims
  Shower Arm Diverters 
 
 
- def format(arr)
+ def checkLayout(arr)
+  html = ""
   arr.each do |e|
     
-
+    outP = "<div class=\"checkbox\">\n
+                                      <label>\n<input type=\"checkbox\" value=\"\" id=\""+e.strip.gsub(' ', '_')+"\">\n"
+    outP += e
+    outP += "</label>\n
+                                  </div>\n"
+    html+=outP
+    html+="\n"
   end
-
+print html
 
  end
