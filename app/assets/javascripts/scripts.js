1d74f51db4516dd32b1190a32cf575d8118a4985
wakelank <wakelank@gmail.com>
2015-12-16 13:44:57 -0500

    Javascript sets tearsheet link on change of the selectors. Link opens with a target blank.

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,17 +1,15 @@
 $(document).on("page:change", (function(){
   function submitTearSheet(){
 //    e.preventDefault();
-    var tearSheetForm = document.forms[0];
+    var tearSheetForm = document.getElementById('tearsheet-form');
     var product_base_number = tearSheetForm.elements["product_base_number"].value;
-    var material_id = tearSheetForm.elements["product[material_id]"].value;
-    var finish_id = tearSheetForm.elements["product[finish_id]"].value;
-    var china_color_id = tearSheetForm.elements["product[china_color_id]"].value;
-    var product_group_id = tearSheetForm.elements["product_group_id"].value;
+    var material_id = tearSheetForm.elements["tearsheet[material_id]"].value;
+    var finish_id = tearSheetForm.elements["tearsheet[finish_id]"].value;
+    var china_color_id = tearSheetForm.elements["tearsheet[china_color_id]"].value;
     var product_data = { "product_base_number" : product_base_number,
                 "material_id" : material_id,
                 "finish_id" : finish_id,
-                "china_color_id" : china_color_id,
-                "product_group_id" : product_group_id
+                "china_color_id" : china_color_id
       }
      $.ajax({
             url: '/products/tearsheet.json',
@@ -25,10 +23,38 @@ $(document).on("page:change", (function(){
     });
   }
 
-     $('.tear-sheet-submit').click(function(e){
-       console.log("clicked");
-       e.preventDefault();
-       submitTearSheet();
-     });
-     console.log('thinggy');
+//# $('.tear-sheet-submit').click(function(e){
+//       console.log("clicked");
+//       e.preventDefault();
+//       submitTearSheet();
+//     });
+     function generate_tearsheet_link(){
+        
+       debugger
+       var tearSheetForm = document.getElementById('tearsheet-form');
+       var product_base_number = tearSheetForm.elements["product_base_number"].value;
+       var material_id = tearSheetForm.elements["tearsheet[material_id]"].value;
+       var finish_id = tearSheetForm.elements["tearsheet[finish_id]"].value;
+       var china_color_id = tearSheetForm.elements["tearsheet[china_color_id]"].value;
+       var product_data = { "product_base_number" : product_base_number,
+         "material_id" : material_id,
+         "finish_id" : finish_id,
+         "china_color_id" : china_color_id
+       }
+      var root_url = window.location.origin
+      var product_id = "3"
+      var tearsheet_number = product_base_number
+
+      var url = root_url + "/tearsheets/" + product_id + "?number=" + tearsheet_number;
+      return url;
+     }
+
+     function set_tearsheet_link(){
+       var $tearsheetTag = $(".tear-sheet-submit");
+       var url = generate_tearsheet_link();
+
+       $tearsheetTag.attr("href", url);
+     }
+
+       $('#tearsheet-form').children().change(set_tearsheet_link);
 }))
