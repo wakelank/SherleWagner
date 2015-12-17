b295453a1d7c857787f879a7dd8057216291d996
wakelank <wakelank@gmail.com>
2015-12-17 13:31:05 -0500

    Product names with links for recently_visited_products appear in footer.

--- app/helpers/recently_viewed_products_helper.rb
+++ app/helpers/recently_viewed_products_helper.rb
@@ -1,10 +1,16 @@
 module RecentlyViewedProductsHelper
 
   def recently_viewed_products
-    recently_viewed_products = []
-    if cookies[:recently_viewed_products] && cookies[:recently_viewed_products].length > 2
+    if there_are_recently_viewed_products?
       recently_viewed_products = JSON.parse(cookies[:recently_viewed_products])
+      recently_viewed_products.map do |recently_viewed_product|
+        Product.find_by(id: recently_viewed_product["product_id"])
+      end
     end
-    recently_viewed_products
   end
+
+  def there_are_recently_viewed_products?
+    cookies[:recently_viewed_products] && cookies[:recently_viewed_products].length > 2
+  end
+
 end
