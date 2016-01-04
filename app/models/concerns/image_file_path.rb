e72bd0fc5233dd3b2686fca2e60bee0636e1bb1f
wakelank <wakelank@gmail.com>
2016-01-04 17:26:17 -0500

    thing

--- app/models/concerns/image_file_path.rb
+++ app/models/concerns/image_file_path.rb
@@ -3,7 +3,7 @@ module ImageFilePath
     def image_file_path
       "/Users/wake/Documents/Work/SherleWagner/images"
         #"/Users/ph1am/Desktop/SW website/images1"
-      #thing
+        #thing
     end
 end
 
