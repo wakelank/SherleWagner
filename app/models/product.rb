361d382b97bd0b04c08f3a1ea0fc436b7d7534fa
wakelank <wakelank@gmail.com>
2016-01-04 16:34:01 -0500

    Saves wake's image file info

--- app/models/product.rb
+++ app/models/product.rb
@@ -33,8 +33,8 @@ class Product < ActiveRecord::Base
         args[:product_type] = ProductType.get_arg row
         args[:product_sub_type] = ProductSubType.get_arg row
         image_name = self.get_image_name_from row
-        images_path = "/Users/ph1am/Desktop/SW website/images1"
-        # images_path = "/Users/wake/Documents/Work/SherleWagner/images"
+        #images_path = "/Users/ph1am/Desktop/SW website/images1"
+         images_path = "/Users/wake/Documents/Work/SherleWagner/images"
         image_file = NullObject.new
         style = Style.get_arg row
         filters = FilterValue.get_arg row
