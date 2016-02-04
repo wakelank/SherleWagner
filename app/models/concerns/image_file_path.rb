6d7a64950fa4bec8b385c31bf527abc7b6130a77
paulh1am <pshiam@gmail.com>
2016-02-04 15:37:12 -0500

    rm binding in file upload mang

--- app/models/concerns/image_file_path.rb
+++ app/models/concerns/image_file_path.rb
@@ -2,7 +2,7 @@ module ImageFilePath
   extend ActiveSupport::Concern
     def image_file_path
         #{}"/Users/wake/Documents/Work/SherleWagner/images"
-       #{}"/Users/ph1am/Desktop/SW_website/images1"
+       "/Users/ph1am/Desktop/SW_website/images1"
        #{}"/home/rails/rails_apps/img_hack"
     end
 end
