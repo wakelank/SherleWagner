0e003d8ddfac59a40f3d88f07134966680fa6e74
paulh1am <pshiam@gmail.com>
2016-02-04 11:45:25 -0500

    img path concern for production

--- app/models/concerns/image_file_path.rb
+++ app/models/concerns/image_file_path.rb
@@ -1,8 +1,9 @@
 module ImageFilePath
   extend ActiveSupport::Concern
     def image_file_path
-        "/Users/wake/Documents/Work/SherleWagner/images"
-       #"/Users/ph1am/Desktop/SW_website/images1"
+        #{}"/Users/wake/Documents/Work/SherleWagner/images"
+       #{}"/Users/ph1am/Desktop/SW_website/images1"
+       "/home/rails/rails_apps/SherleWagner/current/img_file_path"
     end
 end
 
