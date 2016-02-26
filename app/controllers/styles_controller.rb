c8e032a0c3eb60314e8387bf013db2f2a36d99fb
paulh1am <pshiam@gmail.com>
2016-02-26 09:34:27 -0500

    instance var link to image title genre in masonry

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -5,6 +5,10 @@ class StylesController < ApplicationController
     @genres = Genre.all
     
     @pics = Dir.glob("#{Rails.root}/public/images/col_img/*.png")
+    # @contemporary = Genre.where(:name => "Contemporary").pluck(:id).first
+    # @classic = Genre.where(:name => "Classic").pluck(:id).first
+    # @ornate = Genre.where(:name => "Ornate").pluck(:id).first
+    # @traditional = Genre.where(:name => "Traditional").pluck(:id).first
      
   end
   def show
