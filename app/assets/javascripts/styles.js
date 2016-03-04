d46f740f4dab238375b68b0382ef2be198ebb8da
paulh1am <pshiam@gmail.com>
2016-03-04 10:57:03 -0500

    debug masonry3

---
+++ app/assets/javascripts/styles.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+        var $container = $('#styles1');
+        $container.imagesLoaded(function () {
+            $container.masonry({
+                itemSelector:'.box',
+                isAnimated:true,
+                animationOptions:{
+                    duration:750,
+                    easing:'linear',
+                    queue:false
+                }
+            });
+        });
+    })
