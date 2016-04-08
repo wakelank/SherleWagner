54457458b566f26de5045a5463ad4bd7f5cd72ba
wakelank <wakelank@gmail.com>
2016-04-08 10:37:00 -0400

    Rudimentary pg_search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324165452) do
+ActiveRecord::Schema.define(version: 20160408141441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,16 @@ ActiveRecord::Schema.define(version: 20160324165452) do
   end
 
   add_index "other_images", ["product_id"], name: "index_other_images_on_product_id", using: :btree
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "product_components", id: false, force: :cascade do |t|
     t.integer "product_a_id", null: false
