c5c05ed23a645d95fe7d7a284cb806e5a1aac36d
paulh1am <pshiam@gmail.com>
2016-05-10 15:43:16 -0400

    china?

--- app/models/china_color.rb
+++ app/models/china_color.rb
@@ -1,6 +1,7 @@
 class ChinaColor < ActiveRecord::Base
  # has_and_belongs_to_many :china_colored_product_groups, class_name: 'ProductGroups', join_table: 'china_colors_product_groups'
-  has_and_belongs_to_many :china_colored_products, class_name: 'Products', join_table: 'china_colors_products'
+  has_and_belongs_to_many :products
+  # has_and_belongs_to_many :china_colored_products, class_name: 'Products', join_table: 'china_colors_products'
 
 
 
