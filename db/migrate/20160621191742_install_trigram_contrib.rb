0b5c39839b16291189b2e2a6ff2873e021e84a2a
wakelank <wakelank@gmail.com>
2016-06-22 12:46:22 -0400

    removes trigram migration.

--- db/migrate/20160621191742_install_trigram_contrib.rb
+++
@@ -1,9 +0,0 @@
-class InstallTrigramContrib < ActiveRecord::Migration
- def up
-    execute "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-  end
-
-  def down
-    execute "DROP EXTENSION IF EXISTS pg_trgm;"
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621191742) do
+ActiveRecord::Schema.define(version: 20160621155649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_trgm"
 
   create_table "associated_compilations", id: false, force: :cascade do |t|
     t.integer "compilation_a_id", null: false
