c7a08c8ebff61d87f44d39f035d6f1d020d85a2a
wakelank <wakelank@gmail.com>
2016-06-30 15:21:28 -0400

    Removed favorites variable from contact with favorites for testing.

--- app/mailers/contact_with_favorites.rb
+++ app/mailers/contact_with_favorites.rb
@@ -1,7 +1,6 @@
 class ContactWithFavorites < ApplicationMailer
 
-  def email_favorites(favorites:)
-    @favorites = favorites.map(&:number)
+  def email_favorites
     mail(
       to:'wakelank@gmail.com',
       subject: "this is a test",
