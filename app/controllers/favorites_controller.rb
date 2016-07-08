937f1ffa19226e81d4e027a94de3bf106efcfce1
wakelank <wakelank@gmail.com>
2016-07-08 14:28:54 -0400

    flash message for favorites email.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -27,12 +27,18 @@ class FavoritesController < ApplicationController
   end
 
   def send_contact_with_favorites
+    begin
     ContactWithFavorites.email_favorites(favorites: params[:favorites],
-                                        requesters_email: params[:requesters_email],
-                                        requesters_location: params[:requesters_location],
-                                        requesters_comment: params[:requesters_comment]).deliver_now
+                                         requesters_email: params[:requesters_email],
+                                         requesters_location: params[:requesters_location],
+                                         requesters_comment: params[:requesters_comment]).deliver_now
 
-    redirect_to favorites_path
+    flash[:success] = "Your message has been sent. A customer service representative will contact your shortly."
+    rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e 
+      flash.delete :success
+      flash[:danger] = "We're sorry. There was a problem with sending your message. Please call 1-(212)768-3300."
+    end
+  redirect_to favorites_path
   end
 
   private
