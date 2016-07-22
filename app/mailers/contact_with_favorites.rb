dbfda3df452044dd21d22d92e2d4b0a7c31e4bc5
wakelank <wakelank@gmail.com>
2016-07-22 15:42:10 -0400

    Change email message for favorites email.

--- app/mailers/contact_with_favorites.rb
+++ app/mailers/contact_with_favorites.rb
@@ -8,17 +8,16 @@ class ContactWithFavorites < ApplicationMailer
                       requesters_location:,
                       requesters_comment:)
     @favorites = favorites || NullObject.new
-    @requesters_email = requesters_email
-    @requesters_first_name = requesters_first_name
-    @requesters_last_name = requesters_last_name
+    @requesters_email = requesters_email.blank? ? "no email provided" : requesters_email
+    # @requesters_first_name = requesters_first_name
+    # @requesters_last_name = requesters_last_name
     @full_name = requesters_first_name + " " + requesters_last_name
     @additional_recipients = additional_recipients_arr(additional_recipients)
-    @requesters_location = requesters_location
-    @requesters_comment = requesters_comment
+    @requesters_location = requesters_location.blank? ? "no location provided" : requesters_location
+    @requesters_comment = requesters_comment.blank? ? "none" : requesters_comment
     mail(
       to: ['wakelank@gmail.com', 'info@sherlewagner.com', 'pt@sherlewagner.com'] + @additional_recipients,
-      subject: "Customer Inquiry",
-      from: requesters_email
+      subject: "Sherle Wagner Customer Inquiry"
     )
   end
 
